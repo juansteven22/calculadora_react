a8214fdf8bb251ed656c778384727d3e43f15a89
Juan Steven Montenegro Penagos <jusmontenegrope@gmail.com>
2022-06-20 21:58:03 -0500

    first commit

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,8 @@
+import React from "react";
+import '../stylesheets/BotonClear.css'
+const BotonClear = (props) =>(
+    <div className="boton-clear" onClick={props.manejarClear}>
+        {props.children}
+    </div>
+);
+export default BotonClear;
